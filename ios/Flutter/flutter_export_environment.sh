01602201ca67910acb5864d8f9f9d96535b2ad4c
Rodrigo Simões Rosa <rodrigosimoesrosa@gmail.com>
2020-03-09 11:38:31 -0300

    Add files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/rodrigosimoesrosa/Library/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/DATA/flutter/helloworld2/converter"
-export "FLUTTER_TARGET=/Volumes/DATA/flutter/helloworld2/converter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Volumes/DATA/flutter/converter"
+export "FLUTTER_TARGET=/Volumes/DATA/flutter/converter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/rodrigosimoesrosa/Library/flutter/bin/cache/artifacts/engine/ios"
